11470df4d7972d64515662b5f2e5615881e4d982
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2023-05-03 11:05:52 +0530

    commit

--- src/product/ProductCatalouge.js
+++ src/product/ProductCatalouge.js
@@ -1,11 +1,258 @@
-import React from 'react'
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import "./ProductCatalouge.css";
+import Grid from "@mui/material/Unstable_Grid2";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
 
 const ProductCatalouge = () => {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
+
   return (
     <div>
-       <h1>Product catalouage page</h1>
+      <div
+        className="card bghead card-body row noBorder
+            bg-warning"
+      >
+        <div className="mt-4 px-3">
+          <h1 className="display-6 mr"> Product Catalogue</h1>
+          <h6 className="mr3">Select a Product Category to view Plans</h6>
+        </div>
+
+        <div>
+          <form class="form-inline row">
+            <input
+              className="form-control search mr-sm-2 "
+              type="search"
+              placeholder="Search Product"
+              aria-label="Search"
+            />
+            <button className="btn button" type="submit">
+              SEARCH
+            </button>
+          </form>
+        </div>
+        <div className="prdfloat col-12">
+          <Grid className="prdInside" container spacing={1}>
+            {/* //travel insurance */}
+            <Grid xs={2}>
+              <Item className="cardRound">
+                <CardMedia
+                  sx={{ height: 30 }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="green iguana"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="26"
+                  height="26"
+                  fill="purple"
+                  class="bi bi-airplane"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Zm.894.448C7.111 2.02 7 2.569 7 3v4a.5.5 0 0 1-.276.447l-5.448 2.724a.5.5 0 0 0-.276.447v.792l5.418-.903a.5.5 0 0 1 .575.41l.5 3a.5.5 0 0 1-.14.437L6.708 15h2.586l-.647-.646a.5.5 0 0 1-.14-.436l.5-3a.5.5 0 0 1 .576-.411L15 11.41v-.792a.5.5 0 0 0-.276-.447L9.276 7.447A.5.5 0 0 1 9 7V3c0-.432-.11-.979-.322-1.401C8.458 1.159 8.213 1 8 1c-.213 0-.458.158-.678.599Z" />
+                </svg>
+                <CardContent>
+                  <Typography
+                    color="purple"
+                    className="font"
+                    gutterBottom
+                    variant="h9"
+                    component="div"
+                  >
+                    Travel Insurance
+                  </Typography>
+                </CardContent>
+              </Item>
+            </Grid>
+            {/* Health insurance */}
+            <Grid xs={2}>
+              <Item className="cardRound">
+                <CardMedia
+                  sx={{ height: 30 }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="green iguana"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="26"
+                  height="26"
+                  fill="purple"
+                  class="bi bi-activity"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"
+                  />
+                </svg>
+                <CardContent>
+                  <Typography
+                    color="purple"
+                    className="font"
+                    gutterBottom
+                    variant="h9"
+                    component="div"
+                  >
+                    Health Insurance
+                  </Typography>
+                </CardContent>
+              </Item>
+            </Grid>
+            {/* Life insurance */}
+            <Grid xs={2}>
+              <Item className="cardRound">
+                <CardMedia
+                  sx={{ height: 30 }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="green iguana"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="26"
+                  height="26"
+                  fill="purple"
+                  class="bi bi-suit-heart"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
+                </svg>
+                <CardContent>
+                  <Typography
+                    color="purple"
+                    className="font"
+                    gutterBottom
+                    variant="h9"
+                    component="div"
+                  >
+                    Life Insurance
+                  </Typography>
+                </CardContent>
+              </Item>
+            </Grid>
+            {/* Home insurance */}
+            <Grid xs={2}>
+              <Item className="cardRound">
+                <CardMedia
+                  sx={{ height: 30 }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="green iguana"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="26"
+                  height="26"
+                  fill="purple"
+                  class="bi bi-house-door"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
+                </svg>
+                <CardContent>
+                  <Typography
+                    color="purple"
+                    className="font"
+                    gutterBottom
+                    variant="h9"
+                    component="div"
+                  >
+                    Home Insurance
+                  </Typography>
+                </CardContent>
+              </Item>
+            </Grid>
+            {/* Car Insurance */}
+            <Grid xs={2}>
+              <Item className="cardRound">
+                <CardMedia
+                  sx={{ height: 30 }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="green iguana"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="26"
+                  height="26"
+                  fill="purple"
+                  class="bi bi-car-front"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M4 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm10 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2H6ZM4.862 4.276 3.906 6.19a.51.51 0 0 0 .497.731c.91-.073 2.35-.17 3.597-.17 1.247 0 2.688.097 3.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 10.691 4H5.309a.5.5 0 0 0-.447.276Z" />
+                  <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679c.033.161.049.325.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.807.807 0 0 0 .381-.404l.792-1.848ZM4.82 3a1.5 1.5 0 0 0-1.379.91l-.792 1.847a1.8 1.8 0 0 1-.853.904.807.807 0 0 0-.43.564L1.03 8.904a1.5 1.5 0 0 0-.03.294v.413c0 .796.62 1.448 1.408 1.484 1.555.07 3.786.155 5.592.155 1.806 0 4.037-.084 5.592-.155A1.479 1.479 0 0 0 15 9.611v-.413c0-.099-.01-.197-.03-.294l-.335-1.68a.807.807 0 0 0-.43-.563 1.807 1.807 0 0 1-.853-.904l-.792-1.848A1.5 1.5 0 0 0 11.18 3H4.82Z" />
+                </svg>
+                <CardContent>
+                  <Typography
+                    color="purple"
+                    className="font"
+                    gutterBottom
+                    variant="h9"
+                    component="div"
+                  >
+                    Car Insurance
+                  </Typography>
+                </CardContent>
+              </Item>
+            </Grid>
+            {/* Pet insurance */}
+            <Grid xs={2}>
+              <Item className="cardRound ">
+                {/* <Card sx={{ maxWidth: 100 }} > */}
+                <CardMedia
+                  sx={{ height: 30 }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="green iguana"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="26"
+                  height="26"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="purple"
+                    d="M18 4c-1.71 0-2.75.33-3.35.61C13.88 4.23 13 4 12 4s-1.88.23-2.65.61C8.75 4.33 7.71 4 6 4c-3 0-5 8-5 10c0 .83 1.32 1.59 3.14 1.9c.64 2.24 3.66 3.95 7.36 4.1v-4.28c-.59-.37-1.5-1.04-1.5-1.72c0-1 2-1 2-1s2 0 2 1c0 .68-.91 1.35-1.5 1.72V20c3.7-.15 6.72-1.86 7.36-4.1C21.68 15.59 23 14.83 23 14c0-2-2-10-5-10M4.15 13.87c-.5-.12-.89-.26-1.15-.37c.25-2.77 2.2-7.1 3.05-7.5c.54 0 .95.06 1.32.11c-2.1 2.31-2.93 5.93-3.22 7.76M9 12a1 1 0 0 1-1-1c0-.54.45-1 1-1a1 1 0 0 1 1 1c0 .56-.45 1-1 1m6 0a1 1 0 0 1-1-1c0-.54.45-1 1-1a1 1 0 0 1 1 1c0 .56-.45 1-1 1m4.85 1.87c-.29-1.83-1.12-5.45-3.22-7.76c.37-.05.78-.11 1.32-.11c.85.4 2.8 4.73 3.05 7.5c-.25.11-.64.25-1.15.37Z"
+                  />
+                </svg>
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h9"
+                    color="purple"
+                    className="font"
+                    component="div"
+                  >
+                    Pet Insurance
+                  </Typography>
+                </CardContent>
+                {/* </Card> */}
+              </Item>
+            </Grid>
+          </Grid>
+          <br></br>
+          <br></br>
+
+          {/* //------------------------------------------ */}
+
+          <div container className="card card-body">
+            <h1 className="display-6 mr2">Trending Insurance Plans</h1>
+            <h6 className="mr4">
+              Insurance Plans and Policies by CG Insurance
+            </h6>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
+
 }
 
 export default ProductCatalouge
